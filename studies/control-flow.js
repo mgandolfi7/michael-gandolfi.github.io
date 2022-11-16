77b605eda87588d01fed99697f8a19e4a07b790e
mgandolfi7 <michaelgandolfi7@gmail.com>
2022-11-16 14:52:38 +0000

    finished control flow of studies project

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -1,12 +1,53 @@
 /**
- * CONTROL FLOW:
+ * CONTROL FLOW: when you write code, you may want to perform different actions based on different conditions. Enter conditional statements.
  * 
- * a) If
+ * a) if - tests a specific condition to be true or false and if true, the block of code executes. If false it either moves on if there is/are else statements or stops.
  * 
- * b) Else-If
+ * b) else - used with an if statement to execute a block of code if the condition from the previous if statement is false.
  * 
- * c) Else
+ * c) else if - also used with an if statement and can be used with an else if statment. It executes if a second condition is true. If not, it either stops or moves on to the else statment
  * 
- * d) Switch
+ * d) switch - simmilar to if else chains, it tests whether a particular condition is true or false. Can also be chained with multiple switch cases. Remember to include a break after each switch case. Can also have a default case if none of the above cases are true just like an else statement.
  * 
  */
+
+
+// if statements
+var color = 'red';
+
+if (color === 'red') {
+    console.log('the color you picked is red') // will log to the console because the statement is true
+}
+
+if (color === 'black') {
+    console.log('the color you picked is black') // will not log anything to the console because the statement is false
+}
+
+// if else
+if (color === 'green') {
+console.log('the color you picked is green')
+} else {
+    console.log('the color you picked is NOT green') // will log to the console since the if condition is false
+}
+
+// else if
+if (color === 'purple') {
+    console.log('the color you picked is purple')
+} else if (color === 'red') {
+    console.log('the color you picked is red') // will log to the console since the first if statement is false but the else if is true
+}
+
+// switch statements
+switch (color) {
+    case 'red':
+        console.log('the color you picked is red');
+        break;
+    case 'blue':
+        console.log('the color you picked is blue');
+        break;
+    case 'black':
+        console.log('the color you picked is black');
+        break; 
+    default:
+        console.log('the color you picked is not red, blue, or black');
+}
--- studies/loops.js
+++ studies/loops.js
@@ -1,57 +1,50 @@
 /**
- * LOOPS:
+ * LOOPS: - loops are a way for us to iterate over data a set number of times.
  * 
- * a) Loops are a way for us to iterate over data a set number of times
- * 
- * b) For Loops: 
- *      1) Great for iterating over data in an array because we can use it to access an array's index values 
- *      2) use the .length property on an array to get your loop stop condition
+ * a) for loops - reat for iterating over data in an array because we can use it to access an array's index values. Use the .length property on an array to get your loop stop condition.
  * 
  * 
- * c) While Loops:
- *      1) Great for iterating over a set number of times when you know the amount of times
+ * b) while loops - great for iterating over a set number of times when you know the amount of times.
  * 
- * d) For In Loops:
- *      1) great for iterating through an objects keys and or values
+ * c) for in loops - great for iterating through an objects keys and or values.
  * 
  */
 
 // The three types of loops we have learned about are for loops, while loops, and for in loops. Each have their own use cases and best practices.
 
-// Now on to some examples
 
 // Lets say we want to print "Hello World" to the console five times. Instead of typing out console.log("Hello World") five times, we can use a for loop.
 
 for (var i = 1; i <= 5; i++) {
-    console.log('Hello World') // prints 'Hello World' x5
+    console.log('Hello World'); // prints 'Hello World' x5
   }
   
 // Looking inside the parenthesis, it is important to initialize i with the var keyword. It is common practice to use i or "index" for the loop but it will work with any variable name. i = 1 is the starting point of the loop, i <= 5 is the ending point of the loop, and i++ is our counter. In this example every loop or iteration, 1 is added to i until it reaches 5. Now lets say we want to loop over an array of numbers and get the sum.
   
-  var arr = [1,32,5,10,50,22,11]
+  var arr = [1,32,5,10,50,22,11];
   
   var sum = 0
   for (var i = 0; i < arr.length; i++) {
-    sum += arr[i]
+    sum += arr[i];
   }
   
-  console.log(sum) // prints 131
+  console.log(sum); // prints 131
   
 // For this example we are using the .length property to return the number of indexes in the array to be used as our stop condition. Each iteration we are taking sum and adding the current array index's value to it. Now lets say we want to loop over a countdown from 1 - 10 but we want to do it backwards. For this example we will also be using a for loop.
   
   for (var i = 10; i > 0; i--) {
-    console.log(i) // prints 10,9,8, etc.
+    console.log(i); // prints 10,9,8, etc.
   }
   
 // In this loop we start at 10 and end at i > 0 so 1 and instead of incrementing i each loop we decrement i. Now moving onto the while loop. The while loop is great for iterating over something a set number of times. While for loops are great if you don't know the number of times you want to loop, while loops are great if you DO know.
   
-  var arr2 = [10,5,44]
+  var arr2 = [10,5,44];
   
   
   while (arr2.length < 7) {
-    arr2.push(Math.floor((Math.random() * 100)))
+    arr2.push(Math.floor((Math.random() * 100)));
   }
-  console.log(arr2) // prints [10, 5, 44, random1, random2,random3, random4]
+  console.log(arr2); // prints [10, 5, 44, random1, random2,random3, random4]
   
 // In this example we have an array with three values and are going to use a while loop with the condition of the array's length is less than seven. To prevent this from becoming an infinite loop, we will push a random number of 1 - 100 to the array each loop. As soon as the contents of the loop become falsy, the while loop stops. In this case, it stops after the array has seven values. For while loops it's easy to end up with an infinite loop so be careful!
   
@@ -61,9 +54,9 @@ for (var i = 1; i <= 5; i++) {
     familyName: 'Gandolfi',
     age: 24,
     favColor: 'red'
-  }
+  };
   
   for (var keys in michael) {
-    console.log() // prints familyName, age, favColor
+    console.log(); // prints familyName, age, favColor
   }
   
